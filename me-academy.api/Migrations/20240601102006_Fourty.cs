3078bc4612dc51b29a6a678d6276304d8cbf4dac
Mordecai Godwin <mogodwin@axamansard.com>
2024-06-01 11:23:14 +0100

    Patching first release

---
+++ me-academy.api/Migrations/20240601102006_Fourty.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace me_academy.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fourty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TypeId",
+                schema: "dbo",
+                table: "SmeHubs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "SmeHubTypes",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SmeHubTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SmeHubs_TypeId",
+                schema: "dbo",
+                table: "SmeHubs",
+                column: "TypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SmeHubs_SmeHubTypes_TypeId",
+                schema: "dbo",
+                table: "SmeHubs",
+                column: "TypeId",
+                principalSchema: "dbo",
+                principalTable: "SmeHubTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SmeHubs_SmeHubTypes_TypeId",
+                schema: "dbo",
+                table: "SmeHubs");
+
+            migrationBuilder.DropTable(
+                name: "SmeHubTypes",
+                schema: "dbo");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SmeHubs_TypeId",
+                schema: "dbo",
+                table: "SmeHubs");
+
+            migrationBuilder.DropColumn(
+                name: "TypeId",
+                schema: "dbo",
+                table: "SmeHubs");
+        }
+    }
+}
