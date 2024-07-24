799b6d627b79fd1b771fe43e44814dc47d710c1d
Mordecai Godwin <mogodwin@axamansard.com>
2024-07-24 22:38:16 +0100

    Updated coupon validations

--- me-academy.core/Models/Input/Coupons/CouponModel.cs
+++ me-academy.core/Models/Input/Coupons/CouponModel.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using me_academy.core.Models.App.Constants;
 
 namespace me_academy.core.Models.Input.Coupons;
 public class CouponModel
@@ -19,14 +20,18 @@ public class CouponModelValidator : AbstractValidator<CouponModel>
     {
         RuleFor(x => x.Code).NotEmpty().MaximumLength(255);
         RuleFor(x => x.ExpiryDate).GreaterThanOrEqualTo(DateTime.UtcNow);
-        RuleFor(x => x.Amount).GreaterThan(0);
+
         RuleFor(x => x.MinOrderAmount).GreaterThanOrEqualTo(0);
-
-        // Type must be equal to Percentage or Fixed
-        RuleFor(x => x.Type).Must(x => x == "Percentage" || x == "Fixed")
-            .WithMessage("Invalid coupon type.");
-
         // AttachedEmails must be a valid email address
         RuleForEach(x => x.AttachedEmails).EmailAddress();
+
+        // Type must be equal to Percentage or Fixed
+        RuleFor(x => x.Type).Must(x => x == CouponTypes.Percentage || x == CouponTypes.Fixed)
+            .WithMessage($"Invalid coupon type. Only {CouponTypes.Percentage} or {CouponTypes.Fixed} is valid.");
+
+        RuleFor(x => x.Amount)
+            .GreaterThan(0).WithMessage("Amount must be greater than 0.")
+            .LessThanOrEqualTo(100).When(x => x.Type == CouponTypes.Percentage)
+            .WithMessage("Amount must be less than or equal to 100 when type is Percentage.");
     }
 }
